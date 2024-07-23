ab30d42696bc69a29e72d22d2c96f1c81bfc8982
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-07-23 18:14:48 +0500

    1--dars

---
+++ savollarga javob.js
@@ -0,0 +1,7 @@
+1-savol
+//Node.js va console.log orqali
+//c)let va var va const
+//d)o'zgarvchilarni o'zini nomi bilan nomlasa bo'medi 
+//e)alertda qalqibchi oyna document.write esa berilgan narsani sarlavhaga chiqarib berradi
+//f)prompt savol so'rash uchun kere bo'ladi
+//g)+ blan brlashtirish
